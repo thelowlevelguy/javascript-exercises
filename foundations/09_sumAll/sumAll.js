1f7db44d623fa04e2002ce214da1591adf9da963
thelowlevelguy <gkalla013@gmail.com>
2026-06-22 17:29:01 +0000

    sum of numbers between x and y

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+	
+	if (!Number.isInteger(x) || !Number.isInteger(y)){
+		return "ERROR"
+	}
 
+	if (x < 0 || y < 0 ){
+		return "ERROR"
+	}
+
+	const min = Math.min(x, y)
+	const max = Math.max(x, y)
+	
+	//Gauss theoreme : n(x+y) / 2 , where n is the numbers of element(n = x - y + 1 when x and y are inclusive and - 1 otherwise)
+	return ((max-min+1) * (max+min)) / 2
 };
 
 // Do not edit below this line
