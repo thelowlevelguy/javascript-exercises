26f1ef3015de9578ee8d2bc844ab3b7ff4eb3024
thelowlevelguy <gkalla013@gmail.com>
2026-07-02 00:32:50 +0000

    some operation function

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, item) => acc + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((acc, item) => acc * item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+
+		if (a <= 1){
+			return 1
+		}
+
+		let fact = 1
+		let i = 1;
+		while(i <= a){
+			fact *= i
+			i++;
+		}
+		return fact;
 };
 
 // Do not edit below this line
